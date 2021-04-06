e2623c081c24e4d34e23986095858afa27268f22
DESKTOP-4GQEIPC\user <dldncjf14@gmail.com>
2021-04-06 11:50:38 +0900

    woo add. productController

---
+++ src/main/java/product/controller/PWriteContoller.java
@@ -0,0 +1,21 @@
+package product.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PWriteContoller {
+
+	private final String command = "write.prd";
+	private final String getPage = "";
+	private final String gotoPage = "";
+	
+	@RequestMapping(value = command)
+	public ModelAndView doAction() {
+		
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName(getPage);
+		return mav;
+	}
+}
