026d39c3ba56626e2fb5f79228000965b191fbb2
oreobox <ritzbox1121@gmail.com>
2021-04-08 16:07:35 +0900

    main edit

--- src/main/java/main/controller/MainListController.java
+++ src/main/java/main/controller/MainListController.java
@@ -1,13 +1,7 @@
 package main.controller;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import member.model.MemberBean;
 
 @Controller
 public class MainListController {
@@ -15,11 +9,8 @@ public class MainListController {
 	private final String getPage = "mainList";
 	
 	@RequestMapping(value=command)
-	
 	public String doAction() {
 		
-		
 		return getPage;
-		
 	}
 }
